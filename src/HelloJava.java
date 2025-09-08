acacacf4da39c87af4018502ddad33d4c36e068c
annaaa4701 <annaaa4701@gmail.com>
2025-09-08 16:47:12 +0900

    Hello Java

---
+++ src/HelloJava.java
@@ -0,0 +1,8 @@
+/**
+ * A simple program to print "Hello Java" to the console.
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
